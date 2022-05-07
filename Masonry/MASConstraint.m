e2a0104556bfe0fa1c3d7ac218782fd920955eaf
Karl von Randow <karl@xk72.com>
2022-05-08 07:12:28 +1200

    Uninstall constraint when we call inactive in case it is already installed

--- Masonry/MASConstraint.m
+++ Masonry/MASConstraint.m
@@ -63,6 +63,8 @@
 - (MASConstraint * (^)(void))inactive {
     return ^id{
         self.dontInstallImmediately = YES;
+        /* In case the constraint has already been installed */
+        [self uninstall];
         return self;
     };
 }
